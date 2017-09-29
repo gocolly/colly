9f0e0ed83be11bf558f268c25a2e398a5aa81ab6
Adam Tauber <asciimoo@gmail.com>
2017-09-29 21:26:54 +0200

    [enh] add coursera course scraper example

---
+++ examples/coursera_courses/coursera_courses.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/asciimoo/colly"
+)
+
+func main() {
+	c := colly.NewCollector()
+
+	c.AllowedDomains = []string{"coursera.org", "www.coursera.org"}
+	courses := make(map[string]string)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		if e.Attr("class") == "Button_1qxkboh-o_O-primary_cv02ee-o_O-md_28awn8-o_O-primaryLink_109aggg" {
+			return
+		}
+		link := e.Attr("href")
+		if !strings.HasPrefix(link, "/browse") || strings.Index(link, "=signup") > -1 || strings.Index(link, "=login") > -1 {
+			return
+		}
+		if strings.Count("link", "/") > 2 {
+			return
+		}
+		e.Request.Visit(link)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("visiting", r.URL.String())
+	})
+
+	c.OnHTML(`a[name]`, func(e *colly.HTMLElement) {
+		courses[e.Request.AbsoluteURL(e.Attr("href"))] = e.Text
+	})
+
+	c.Visit("https://coursera.org/browse")
+
+	for url, title := range courses {
+		fmt.Println(url, "-", title)
+	}
+}
