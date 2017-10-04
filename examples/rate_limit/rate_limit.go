d0ccd8f6788e9c6ca5d167dfd9d2c59fca003fb0
Adam Tauber <asciimoo@gmail.com>
2017-10-05 00:16:15 +0200

    [enh] add rate limit example

---
+++ examples/rate_limit/rate_limit.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	//"time"
+
+	"github.com/asciimoo/colly"
+)
+
+func main() {
+	url := "https://httpbin.org/delay/2"
+
+	c := colly.NewCollector()
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*httpbin.*",
+		Parallelism: 2,
+		//Delay:      5 * time.Second,
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Starting", r.URL, time.Now())
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL, time.Now())
+	})
+
+	for i := 0; i < 4; i++ {
+		go c.Visit(fmt.Sprintf("%s?n=%d", url, i))
+	}
+	c.Visit(url)
+	c.Wait()
+}
