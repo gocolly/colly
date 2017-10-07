8f983aadf454baa4b1ab6404877375539dbb127f
Noémi Ványi <sitbackandwait@gmail.com>
2017-10-08 01:09:39 +0200

    add comments to examples

--- examples/coursera_courses/coursera_courses.go
+++ examples/coursera_courses/coursera_courses.go
@@ -9,32 +9,45 @@ import (
 )
 
 func main() {
+	// Instantiate default collector
 	c := colly.NewCollector()
 
+	// Visit only domains: coursera.org, www.coursera.org
 	c.AllowedDomains = []string{"coursera.org", "www.coursera.org"}
 	courses := make(map[string]string)
 
+	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// If attirbute class of a is this long string return from callback
+		// As this a is irrelevant
 		if e.Attr("class") == "Button_1qxkboh-o_O-primary_cv02ee-o_O-md_28awn8-o_O-primaryLink_109aggg" {
 			return
 		}
 		link := e.Attr("href")
+		// If link start with browse or includes either signup or login return from callback
 		if !strings.HasPrefix(link, "/browse") || strings.Index(link, "=signup") > -1 || strings.Index(link, "=login") > -1 {
 			return
 		}
+		// start scaping the page under the link found
 		e.Request.Visit(link)
 	})
 
+	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		log.Println("visiting", r.URL.String())
 	})
 
+	// On every a HTML element which has name attribute call callback
 	c.OnHTML(`a[name]`, func(e *colly.HTMLElement) {
+		// Add to courses map where key is the absolute URL and the
+		// values is the name of the course
 		courses[e.Request.AbsoluteURL(e.Attr("href"))] = e.Text
 	})
 
+	// Start scraping on http://coursera.com/browse
 	c.Visit("https://coursera.org/browse")
 
+	// After scraping is finished print the results
 	for url, title := range courses {
 		fmt.Println(url, "-", title)
 	}
--- examples/rate_limit/rate_limit.go
+++ examples/rate_limit/rate_limit.go
@@ -10,25 +10,33 @@ import (
 func main() {
 	url := "https://httpbin.org/delay/2"
 
+	// Instantiate default collector
 	c := colly.NewCollector()
 
+	// Limit the number of threads started by colly to two
+	// when visting links which domains' matches "*httpbin.*" glob
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*httpbin.*",
 		Parallelism: 2,
 		//Delay:      5 * time.Second,
 	})
 
+	// Before making a request print "Starting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Starting", r.URL, time.Now())
 	})
 
+	// After making a request print "Finished ..."
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL, time.Now())
 	})
 
+	// Start scraping in four threads on https://httpbin.org/delay/2
 	for i := 0; i < 4; i++ {
 		go c.Visit(fmt.Sprintf("%s?n=%d", url, i))
 	}
+	// Start scraping on https://httpbin.org/delay/2
 	c.Visit(url)
+	// Wait until threads are finished
 	c.Wait()
 }
