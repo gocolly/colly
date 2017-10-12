17408908ed22b86771f6e364879e2321b7ca7d8e
Adam Tauber <asciimoo@gmail.com>
2017-10-12 15:53:50 +0200

    [enh] add basic tests

---
+++ colly_test.go
@@ -0,0 +1,117 @@
+package colly
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+)
+
+var testServerAddr string = "127.0.0.1:31337"
+var testServerRootURL string = fmt.Sprintf("http://%s/", testServerAddr)
+var serverIndexResponse []byte = []byte("hello world\n")
+
+func init() {
+	srv := &http.Server{Addr: testServerAddr}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(serverIndexResponse)
+	})
+
+	http.HandleFunc("/html", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Conent-Type", "text/html")
+		w.Write([]byte(`<!DOCTYPE html>
+<html>
+<head>
+<title>Test Page</title>
+</head>
+<body>
+<h1>Hello World</h1>
+<p class="description">This is a test page</p>
+<p class="description">This is a test paragraph</p>
+</body>
+</html>
+		`))
+	})
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("Httpserver: ListenAndServe() error: %s", err)
+		}
+	}()
+}
+
+func TestCollectorVisit(t *testing.T) {
+	c := NewCollector()
+
+	onRequestCalled := false
+	onResponseCalled := false
+
+	c.OnRequest(func(r *Request) {
+		onRequestCalled = true
+		r.Ctx.Put("x", "y")
+	})
+
+	c.OnResponse(func(r *Response) {
+		onResponseCalled = true
+
+		if r.Ctx.Get("x") != "y" {
+			t.Error("Failed to retrieve context value for key 'x'")
+		}
+
+		if !bytes.Equal(r.Body, serverIndexResponse) {
+			t.Error("Response body does not match with the original content")
+		}
+	})
+
+	c.Visit(testServerRootURL)
+
+	if !onRequestCalled {
+		t.Error("Failed to call OnRequest callback")
+	}
+
+	if !onResponseCalled {
+		t.Error("Failed to call OnResponse callback")
+	}
+}
+
+func TestCollectorOnHTML(t *testing.T) {
+	c := NewCollector()
+
+	titleCallbackCalled := false
+	paragraphCallbackCount := 0
+
+	c.OnHTML("title", func(e *HTMLElement) {
+		titleCallbackCalled = true
+		if e.Text != "Test Page" {
+			t.Error("Title element text does not match, got", e.Text)
+		}
+	})
+
+	c.OnHTML("p", func(e *HTMLElement) {
+		paragraphCallbackCount += 1
+		if e.Attr("class") != "description" {
+			t.Error("Failed to get paragraph's class attribute")
+		}
+	})
+
+	c.Visit(testServerRootURL + "/html")
+
+	if !titleCallbackCalled {
+		t.Error("Failed to call OnHTML callback for <title> tag")
+	}
+
+	if paragraphCallbackCount != 2 {
+		t.Error("Failed to find all <p> tags")
+	}
+}
+
+func BenchmarkVisit(b *testing.B) {
+	c := NewCollector()
+	c.OnHTML("p", func(_ *HTMLElement) {})
+
+	for n := 0; n < b.N; n++ {
+		c.Visit(fmt.Sprintf("%shtml?q=%d", testServerRootURL, n))
+	}
+}
