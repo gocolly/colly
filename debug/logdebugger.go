25bffd891805f7d6161937e24c87565fd984edd1
Adam Tauber <asciimoo@gmail.com>
2017-11-17 12:29:51 +0100

    [enh] add ability to set custom output and flags to logdebugger

--- debug/logdebugger.go
+++ debug/logdebugger.go
@@ -1,13 +1,23 @@
 package debug
 
 import (
+	"io"
 	"log"
+	"os"
 	"sync/atomic"
 	"time"
 )
 
 // LogDebugger is the simplest debugger which prints log messages to the STDERR
 type LogDebugger struct {
+	// Output is the log destination, anything can be used which implements them
+	// io.Writer interface. Leave it blank to use STDERR
+	Output io.Writer
+	// Prefix appears at the beginning of each generated log line
+	Prefix string
+	// Flag defines the logging properties.
+	Flag    int
+	logger  *log.Logger
 	counter int32
 	start   time.Time
 }
@@ -16,11 +26,15 @@ type LogDebugger struct {
 func (l *LogDebugger) Init() error {
 	l.counter = 0
 	l.start = time.Now()
+	if l.Output == nil {
+		l.Output = os.Stderr
+	}
+	l.logger = log.New(l.Output, l.Prefix, l.Flag)
 	return nil
 }
 
 // Event receives Collector events and prints them to STDERR
 func (l *LogDebugger) Event(e *Event) {
 	i := atomic.AddInt32(&l.counter, 1)
-	log.Printf("[%06d] %d [%6d - %s] %q (%s)\n", i, e.CollectorId, e.RequestId, e.Type, e.Values, time.Since(l.start))
+	l.logger.Printf("[%06d] %d [%6d - %s] %q (%s)\n", i, e.CollectorId, e.RequestId, e.Type, e.Values, time.Since(l.start))
 }
