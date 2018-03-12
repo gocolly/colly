33beeb8c199bac291bc1b8220153d433720c0e2b
Adam Tauber <asciimoo@gmail.com>
2018-03-12 20:18:18 +0100

    [enh] add chrome user agents and update firefox versions

--- extensions/random_user_agent.go
+++ extensions/random_user_agent.go
@@ -9,6 +9,7 @@ import (
 
 var uaGens = []func() string{
 	genFirefoxUA,
+	genChromeUA,
 }
 
 // RandomUserAgent generates a random browser user agent on every request
@@ -19,16 +20,27 @@ func RandomUserAgent(c *colly.Collector) {
 }
 
 var ffVersions = []float32{
+	58.0,
+	57.0,
+	56.0,
+	52.0,
+	48.0,
 	40.0,
-	39.0,
-	38.0,
-	37.0,
-	36.0,
 	35.0,
 }
 
-var ffOSs = []string{
+var chromeVersions = []string{
+	"65.0.3325.146",
+	"64.0.3282.0",
+	"41.0.2228.0",
+	"40.0.2214.93",
+	"37.0.2062.124",
+}
+
+var osStrings = []string{
 	"Macintosh; Intel Mac OS X 10_10",
+	"Windows NT 10.0",
+	"Windows NT 5.1",
 	"Windows NT 6.1; WOW64",
 	"Windows NT 6.1; Win64; x64",
 	"X11; Linux x86_64",
@@ -36,6 +48,12 @@ var ffOSs = []string{
 
 func genFirefoxUA() string {
 	version := ffVersions[rand.Intn(len(ffVersions))]
-	os := ffOSs[rand.Intn(len(ffOSs))]
+	os := osStrings[rand.Intn(len(osStrings))]
 	return fmt.Sprintf("Mozilla/5.0 (%s; rv:%.1f) Gecko/20100101 Firefox/%.1f", os, version, version)
+}
+
+func genChromeUA() string {
+	version := chromeVersions[rand.Intn(len(chromeVersions))]
+	os := osStrings[rand.Intn(len(osStrings))]
+	return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36", os, version)
 }
