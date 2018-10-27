9d640534f8039562147922ba91595fc4f3e0817d
Nigel Babu <nigelb@redhat.com>
2018-10-27 17:55:45 +0530

    Increment i before return True
    The counter does not get incremented at all at the moment

--- htmlelement.go
+++ htmlelement.go
@@ -111,9 +111,9 @@ func (h *HTMLElement) ForEachWithBreak(goquerySelector string, callback func(int
 	h.DOM.Find(goquerySelector).EachWithBreak(func(_ int, s *goquery.Selection) bool {
 		for _, n := range s.Nodes {
 			if callback(i, NewHTMLElementFromSelectionNode(h.Response, s, n, i)) {
+			i++
 				return true
 			}
-			i++
 		}
 		return false
 	})
