9a7a9966a1a89b1454d0c0de9b16d6ccadc2f488
Vladislav <vla-dy@yandex.ru>
2020-12-17 13:58:03 +0300

    SetProxyFunc: fix proxies rotation

--- colly.go
+++ colly.go
@@ -1012,9 +1012,11 @@ func (c *Collector) SetProxyFunc(p ProxyFunc) {
 	t, ok := c.backend.Client.Transport.(*http.Transport)
 	if c.backend.Client.Transport != nil && ok {
 		t.Proxy = p
+		t.DisableKeepAlives = true
 	} else {
 		c.backend.Client.Transport = &http.Transport{
-			Proxy: p,
+			Proxy:             p,
+			DisableKeepAlives: true,
 		}
 	}
 }
